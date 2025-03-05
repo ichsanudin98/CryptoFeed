2f3edefb464ea08148e42af7add002ab9712312e
ichsan <ichsanudin.chairin98@gmail.com>
2025-03-05 11:50:47 +0700

    Create `CryptoFeedUseCase` interface boundary

---
+++ app/src/main/java/com/example/cryptofeed/CryptoFeedUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.cryptofeed
+
+import kotlinx.coroutines.flow.Flow
+
+sealed class CryptoFeedResult {
+    data class Success(val cryptoFeedItems: List<CryptoFeed>): CryptoFeedResult()
+    data class Error(val ex: Exception): CryptoFeedResult()
+}
+
+interface CryptoFeedUseCase {
+    fun load(): Flow<CryptoFeedResult>
+}
