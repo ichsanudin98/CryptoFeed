827a19bc7b9d406f7eda40201a3b61965ae97b65
ichsan <ichsanudin.chairin98@gmail.com>
2025-03-14 11:20:40 +0700

    `LoadCryptoFeedRemoteUseCase` doesn't requet data upon creation

--- app/src/test/java/com/example/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/example/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,10 +1,27 @@
 package com.example.cryptofeed
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
+
+// TDD membantu kita dalam
+// 1. getting fast feedback
+// 2. kita tau apa yang kita expect
+
+// sut = system under test
+
+class LoadCryptoFeedRemoteUseCase {
+
+}
+
+class HttpClient {
+    var count = 0
+}
 
 class LoadCryptoFeedRemoteUseCaseTest {
     @Test
-    fun testInit() {
-
+    fun testInitDoesNotLoad() {
+        val client = HttpClient()
+        LoadCryptoFeedRemoteUseCase()
+        assertTrue(client.count == 0)
     }
 }
