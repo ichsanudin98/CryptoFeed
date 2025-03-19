ebcfbc02dfd1ff878b1a76507bef49766dad4a66
ichsan <ichsanudin.chairin98@gmail.com>
2025-03-19 17:34:03 +0700

    Make instance of `HttpClient` is mutable

--- app/src/test/java/com/example/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/example/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,5 +1,6 @@
 package com.example.cryptofeed
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -10,18 +11,41 @@ import org.junit.Test
 // sut = system under test
 
 class LoadCryptoFeedRemoteUseCase {
-
+    fun load() {
+        HttpClient.instance.count = 1
+    }
 }
 
-class HttpClient {
+// singleton below
+class HttpClient private constructor() {
+    companion object {
+        val instance = HttpClient()
+    }
     var count = 0
 }
+
+/*class HttpClient {
+    var count = 0
+}*/
 
 class LoadCryptoFeedRemoteUseCaseTest {
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClient()
+        val client = HttpClient.instance
         LoadCryptoFeedRemoteUseCase()
         assertTrue(client.count == 0)
+    }
+
+    @Test
+    fun testLoadRequestData() {
+        // Given (Component and dependencies)
+        val client = HttpClient.instance
+        val sut = LoadCryptoFeedRemoteUseCase()
+
+        // When (Action)
+        sut.load()
+
+        // Then
+        assertEquals(1, client.count)
     }
 }
