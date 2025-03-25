8a01869d2bfc6fdaec2dcdae9ff7d6aab2a8cc80
ichsan <ichsanudin.chairin98@gmail.com>
2025-03-25 10:42:14 +0700

    Move test logic to separate  type `HttpClientSpy`

--- app/src/test/java/com/example/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/example/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -10,18 +10,43 @@ import org.junit.Test
 
 // sut = system under test
 
-class LoadCryptoFeedRemoteUseCase {
+class LoadCryptoFeedRemoteUseCase constructor(
+    private val client: HttpClient
+)  {
     fun load() {
-        HttpClient.instance.count = 1
+        client.get()
     }
 }
 
 // singleton below
-class HttpClient private constructor() {
+// Syarat kelas singleton ada 2
+// private dan mutable
+/*class HttpClient private constructor() {
     companion object {
         val instance = HttpClient()
     }
     var count = 0
+}*/
+
+// Global instance
+// Jika global ini sudah dipakai dibanyak tempat, maka menjadi global mutable shared instance
+open class HttpClient {
+    companion object {
+        var instance = HttpClient()
+    }
+
+    open fun get() {
+
+    }
+}
+
+class HttpClientSpy: HttpClient() {
+    var getCount = 0
+
+    override fun get() {
+        super.get()
+        getCount++
+    }
 }
 
 /*class HttpClient {
@@ -31,21 +56,23 @@ class HttpClient private constructor() {
 class LoadCryptoFeedRemoteUseCaseTest {
     @Test
     fun testInitDoesNotLoad() {
-        val client = HttpClient.instance
-        LoadCryptoFeedRemoteUseCase()
-        assertTrue(client.count == 0)
+        val client = HttpClientSpy()
+        HttpClient.instance = client
+        LoadCryptoFeedRemoteUseCase(client)
+        assertTrue(client.getCount == 0)
     }
 
     @Test
     fun testLoadRequestData() {
         // Given (Component and dependencies)
-        val client = HttpClient.instance
-        val sut = LoadCryptoFeedRemoteUseCase()
+        val client = HttpClientSpy()
+        HttpClient.instance = client
+        val sut = LoadCryptoFeedRemoteUseCase(client)
 
         // When (Action)
         sut.load()
 
         // Then
-        assertEquals(1, client.count)
+        assertEquals(1, client.getCount)
     }
 }
