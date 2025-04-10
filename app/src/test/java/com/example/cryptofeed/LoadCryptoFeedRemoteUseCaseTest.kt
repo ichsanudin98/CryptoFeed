88d6dc08667e2c4248e976f1770815d7fe4e8fa2
ichsan <ichsanudin.chairin98@gmail.com>
2025-04-10 16:41:05 +0700

    Move `LoadCryptoFeedRemoteUseCase` components to production

--- app/src/test/java/com/example/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
+++ app/src/test/java/com/example/cryptofeed/LoadCryptoFeedRemoteUseCaseTest.kt
@@ -1,53 +1,10 @@
 package com.example.cryptofeed
 
+import com.example.cryptofeed.api.HttpClient
+import com.example.cryptofeed.api.LoadCryptoFeedRemoteUseCase
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
-
-// TDD membantu kita dalam
-// 1. getting fast feedback
-// 2. kita tau apa yang kita expect
-
-// sut = system under test
-
-class LoadCryptoFeedRemoteUseCase constructor(
-    private val client: HttpClient
-)  {
-    fun load() {
-        client.get()
-    }
-}
-
-// singleton below
-// Syarat kelas singleton ada 2
-// private dan mutable
-/*class HttpClient private constructor() {
-    companion object {
-        val instance = HttpClient()
-    }
-    var count = 0
-}*/
-
-// Global instance
-// Jika global ini sudah dipakai dibanyak tempat, maka menjadi global mutable shared instance
-/*open class HttpClient {
-    companion object {
-        var instance = HttpClient()
-    }
-
-    open fun get() {
-
-    }
-}*/
-interface HttpClient {
-    fun get() {
-
-    }
-}
-
-/*class HttpClient {
-    var count = 0
-}*/
 
 class LoadCryptoFeedRemoteUseCaseTest {
     @Test
@@ -58,13 +15,8 @@ class LoadCryptoFeedRemoteUseCaseTest {
 
     @Test
     fun testLoadRequestData() {
-        // Given (Component and dependencies)
         val (sut, client) = makeSut()
-
-        // When (Action)
         sut.load()
-
-        // Then
         assertEquals(1, client.getCount)
     }
 
