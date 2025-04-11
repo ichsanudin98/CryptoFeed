0ca19e2f652a4e34d0883de8e0fc1d4b515ca601
ichsan <ichsanudin.chairin98@gmail.com>
2025-04-11 17:43:01 +0700

    Add mockk dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    testImplementation(libs.mockk.android)
+    testImplementation(libs.mockk.agent)
 }
